bc7293ade53f918695cf7495d4493003dbc563f9
jaga12543 <117670929+jaga12543@users.noreply.github.com>
2024-06-08 12:16:13 +0530

    .myd.sql

--- .myd.sql
+++ .myd.sql
@@ -1,7 +1,6 @@
 USE employees;
-
 CREATE TABLE profile (
-    employee_id VARCHAR(50) PRIMARY KEY,
-    first_name VARCHAR(50) NOT NULL,
-    last_name VARCHAR(50) NOT NULL
+employee_id VARCHAR(50)
+first_name VARCHAR(50) 
+last_name VARCHAR(50) 
 );
